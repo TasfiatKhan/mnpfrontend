dc5df82c53b962522134e8f03a1c4e33b657fda3
Tasfiat-dev <69678164+Tasfiat-dev@users.noreply.github.com>
2022-02-19 05:11:20 +0600

    homepage, login page, debugging logout button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Notes from './components/Notes';
+import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import PrivateRoute from './utils/PrivateRoute';
+import LoginPage from './components/LoginPage';
+import { AuthProvider } from './utils/AuthContext';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Notes/>
-      <Footer/>
+      <Router>
+        <AuthProvider>
+          <Header/>
+          <PrivateRoute component={HomePage} path="/" exact/>
+          <Route component={LoginPage} path="/login"/>
+          <Footer/>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
